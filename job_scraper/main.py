b9e804535e93bc1b997886675a76c7001c1393c3
tomvothecoder <tomvothecoder@gmail.com>
2021-04-17 11:46:29 -0700

    Refactor fields and parsing

--- job_scraper/main.py
+++ job_scraper/main.py
@@ -10,35 +10,22 @@ from bs4 import BeautifulSoup as bs
 logger = logging.getLogger(__name__)
 
 
-def scrape_indeed(pages: List[int] = None):
-    """
-    Performs the web scrapping process using BeautifulSoup4 and pandas
-    for data processing. The web page is fetched using the request
-    library. Each job posting on the page are stored as
-    'containers' with a class of 'row'. After fetching each container,
-    perform parsing the fields of a post.
-    """
-    scraper = IndeedScraper(pages=pages)
-    scraper.scrape()
-    # scraper.save_posts()
-
-
-def parse_date(date_posted: str) -> Optional[str]:
+def parse_date(days_ago: str) -> Optional[str]:
     """Parses the date field based to return the correct value.
     If the value is 'Just Posted' or 'Today', set day = 0.
     """
-    if not date_posted:
+    if not days_ago:
         return None
 
-    if date_posted in {"Just posted", "Active today", "Today"}:
+    if days_ago in {"Just posted", "Active today", "Today"}:
         days_ago = "0"
     else:
-        days_ago = date_posted.replace("Active", "")
+        days_ago = days_ago.replace("Active", "")
 
     return days_ago
 
 
-def find_span(container: bs, tag: str, class_: str) -> Union[str, None]:
+def extract_html_tag(container: bs, tag: str, class_: str) -> Union[str, None]:
     """Searches a specified tag in a job post container.
     If the tag is found, return the stripped version of the text.
     Otherwise return None.
@@ -63,21 +50,19 @@ class IndeedScraper:
         # self.pages = pages if pages else range(0, 1001, 10)
         self.pages = [0, 10]
 
-        # Stores the posts
         self.fields: List[str] = [
             "title",
             "company",
-            "description",
-            "salary",
             "location",
-            "date_posted",
-            "date_added_db",
+            "description",
+            "days_ago",
+            "salary",
             "link",
-            "position",
         ]
+
         self.df: pd.DataFrame = pd.DataFrame(columns=self.fields)
 
-    def generate_url(self, job_title: str) -> str:
+    def generate_url(self, job_title: str, page: int) -> str:
         """Generates the Indeed query URL
 
         :param job_title: Title of the job
@@ -86,7 +71,7 @@ class IndeedScraper:
         :rtype: str
         """
 
-        url = f"https://www.indeed.com/jobs?q={job_title}&l={self.location}"
+        url = f"https://www.indeed.com/jobs?q={job_title}&l={self.location}&sort=date&start={page}"
         return url
 
     def scrape(self):
@@ -94,82 +79,82 @@ class IndeedScraper:
 
         for job_title in self.job_titles:
             for page in self.pages:
-                url = self.generate_url(job_title)
+                url = self.generate_url(job_title, page)
                 page_html = requests.get(url)
-                soup = bs(page_html.content, "html.parser")
-                post_container = soup.findAll("div", {"class": "row"})
-                self.parse_container(post_container)
 
-        self.parse_posts()
+                if page_html:
+                    soup = bs(page_html.text, "html.parser")
+                    containers = soup.findAll("div", "jobsearch-SerpJobCard")
+                    self.parse_containers(containers)
+                else:
+                    raise TypeError(page_html)
 
-    def parse_container(self, containers: List[bs]):
+        self.post_processing()
+        self.save_posts()
+
+    def parse_containers(self, containers: List[bs]):
         """Parses the container with the job information
 
         :param containers: Job containers
         :type containers: List[bs]
         """
 
-        today = date.today()
-        span_fields = {"company", "location", "date"}
-
         for container in containers:
-            post_href = container.find("a", {"class": "jobtitle"})["href"]
+            fields: Dict[str, Any] = {field: None for field in self.fields}
 
-            fields: Dict[str, Any] = {
-                f: find_span(container, "span", f) for f in span_fields
-            }
+            a_tag = container.h2.a
+            post_url = a_tag.get("href")
+            fields["link"] = f"https://indeed.com/{post_url}"
 
-            fields.update(
-                {
-                    "date_posted": parse_date(fields["date"]),
-                    "title": container.a.text,
-                    "date_added_db": today,
-                    "description": find_span(container, "div", class_="summary"),
-                    "link": f"https://indeed.com/{post_href}",
-                }
-            )
+            try:
+                fields["title"] = a_tag.get("title")
+            except AttributeError:
+                fields["title"] = None
+            try:
+                fields["company"] = container.find("span", "company").text.strip()
+            except AttributeError:
+                fields["company"] = None
+            try:
+                fields["location"] = container.find("div", "recJobLoc").get(
+                    "data-rc-loc"
+                )
+            except AttributeError:
+                fields["location"] = None
+            try:
+                fields["description"] = container.find("div", "summary").text.strip()
+            except AttributeError:
+                fields["description"] = None
+            try:
+                fields["days_ago"] = container.find("span", "date").text.strip()
+            except AttributeError:
+                fields["days_ago"] = None
+            try:
+                fields["salary"] = container.find("span", "salaryText").text.strip()
+            except AttributeError:
+                fields["salary"] = None
 
             self.df = self.df.append(fields, ignore_index=True)
 
-    def parse_posts(self):
+    def post_processing(self):
         """Parses the dataframe containing all of the job posts.
+
         The first step is to remove companies that are spam, which is
         mainly 'Indeed Prime'. Afterwards, duplicate entries are dropped
-        based on the 'company', 'date_posted', and 'title' fields. Often
+        based on the 'company', 'days_ago', and 'title' fields. Often
         times there are multiple of the same job posting listed on
         different days which is not useful to search through for the end-user.
         """
         logger.info("Parsing posts")
 
-        self.df.title = self.df.title.str.strip()
-
-        spam_companies = ["Indeed Prime"]
-        self.df = self.df[~self.df["company"].isin(spam_companies)]
         self.df = self.df.dropna(subset=["company"])
-        self.df = self.df.drop_duplicates(subset=["company", "date_posted", "title"])
+        self.df = self.df.drop_duplicates(subset=["company", "days_ago", "title"])
 
-    # def save_posts(self):
-    #     """Saves each dataframe row as a record using get_or_create()."""
-    #     logger.info("Savings posts to database")
-    #     records = self.df.to_dict("records")
-
-    #     for record in records:
-    #         Company.objects.get_or_create(name=record["company"])
-
-    #         Post.objects.get_or_create(
-    #             title=record["title"],
-    #             company_id=record["company"],
-    #             defaults={
-    #                 "date_posted": record["date_posted"],
-    #                 "description": record["description"],
-    #                 "location": record["location"],
-    #                 "is_sponsored": False,
-    #                 "date_added_db": record["date_added_db"],
-    #                 "source_id": record["source"],
-    #                 "link": record["link"],
-    #             },
-    #         )
+    def save_posts(self):
+        """Saves each dataframe row as a record using get_or_create()."""
+        today = date.today().strftime("%m_%d_%y")
+        self.df.to_csv(f"jobs_{today}.csv")
+        self.df.to_excel(f"jobs_{today}.xlsx")
 
 
 if __name__ == "__main__":
-    scraper = scrape_indeed()
+    scraper = IndeedScraper()
