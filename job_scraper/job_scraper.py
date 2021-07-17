b151cc14cf6f1281786fe159fef2cfabe8fef435
tomvothecoder <tomvothecoder@gmail.com>
2021-07-17 14:55:34 -0700

    Update scraper with latest HTML tags and classes - Add python-dotenv to read OUTPUT_FILE env - Updating VSCode python settings in settings.json

--- job_scraper/job_scraper.py
+++ job_scraper/job_scraper.py
@@ -1,5 +1,4 @@
 import logging
-from datetime import datetime
 from random import random
 from time import sleep
 from typing import TYPE_CHECKING, Dict, List, Optional, Tuple, TypedDict, Union
@@ -7,6 +6,7 @@ from typing import TYPE_CHECKING, Dict, List, Optional, Tuple, TypedDict, Union
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
+from dotenv import dotenv_values
 from tqdm import tqdm
 from typing_extensions import Literal
 
@@ -15,11 +15,14 @@ logger = logging.getLogger(__name__)
 if TYPE_CHECKING:
     from bs4.element import PageElement, ResultSet
 
+config = dotenv_values(".env")
+
 
 class IndeedScraper:
     """A class representing an Indeed scraper."""
 
-    base_url = "https://indeed.com"
+    BASE_URL = "https://indeed.com"
+    OUTPUT_FILE = config["OUTPUT_FILE"]
 
     def __init__(
         self,
@@ -55,42 +58,45 @@ class IndeedScraper:
                     break
 
                 soup = bs(page_html.text, "html.parser")
-                cards = soup.find_all("div", "jobsearch-SerpJobCard")
-                self.parse_cards(cards)
+                cards = soup.find_all("a", "result")
+                self.parse_cards_to_df(cards)
 
                 try:
                     pagination = soup.find("a", {"aria-label": "Next"}).get("href")
-                    url = IndeedScraper.base_url + pagination
+                    url = IndeedScraper.BASE_URL + pagination
 
                     # Wait a random number of seconds between scrapping
                     wait_time = random() * 10
                     print(f"Waiting {wait_time} before scrapping again...")
                     sleep(wait_time)
                 except AttributeError:
+                    # Reached the end
                     break
 
-        self.post_processing()
-        self.save_posts()
+        self.post_process_df()
+        self.save()
 
     def generate_url(self, job_title: str) -> str:
-        """Generates the Indeed query URL
+        """Generates the Indeed query URL.
 
         :param job_title: Title of the job
         :type job_title: str
         :return: Indeed query URL
         :rtype: str
         """
-        url = f"{IndeedScraper.base_url}/jobs?q={job_title}&l={self.location}&sort={self.sort_by}"
-
+        url = (
+            f"{IndeedScraper.BASE_URL}/jobs?q={job_title}"
+            f"&l={self.location}"
+            f"&sort={self.sort_by}"
+        )
         if self.exp_lvl:
             url += f"&explvl={self.exp_lvl}"
-
         if self.radius:
             url += f"&radius={self.radius}"
 
         return url
 
-    def parse_cards(self, cards: "ResultSet"):
+    def parse_cards_to_df(self, cards: "ResultSet"):
         """Parses cards containing the job post information.
 
         :param cards: A ResultSet of PageElements.
@@ -100,13 +106,13 @@ class IndeedScraper:
         job_posts: List[Dict[str, Optional[str]]] = []
 
         for card in cards:
-            job_post = self.parse_html(card)
+            job_post = self.parse_card_html(card)
             job_posts.append(job_post)
 
         self.df = self.df.append(job_posts, ignore_index=True)
 
-    def parse_html(self, card: "PageElement") -> Dict[str, Optional[str]]:
-        """Parses the raw HTML job card for the target text inside a matched HTML tag and class.
+    def parse_card_html(self, card: "PageElement") -> Dict[str, Optional[str]]:
+        """Parses job card HTML for text matching HTML tag and class.
 
         :param card: Contains the HTML text for the job card
         :type card: PageElement
@@ -115,14 +121,7 @@ class IndeedScraper:
         """
 
         job_post: Dict[str, Optional[str]] = {}
-
-        # Parse anchor tag fields first
-        anchor_tag = card.h2.a
-        job_post["url"] = f"{IndeedScraper.base_url}{anchor_tag.get('href')}"
-        try:
-            job_post["title"] = anchor_tag.get("title")
-        except AttributeError:
-            job_post["title"] = None
+        job_post["url"] = f"{IndeedScraper.BASE_URL}{card.get('href')}"
 
         HTML_ELEMENT = TypedDict(
             "HTML_ELEMENT",
@@ -134,11 +133,13 @@ class IndeedScraper:
 
         # Parse additional fields
         fields: Dict[str, HTML_ELEMENT] = {
-            "company": {"html_tag": "span", "html_class": "company"},
-            "location": {"html_tag": "div", "html_class": ("recJobLoc", "data-rc-loc")},
-            "description": {"html_tag": "div", "html_class": "summary"},
+            "title": {"html_tag": "h2", "html_class": "jobTitle"},
+            "company": {"html_tag": "span", "html_class": "companyName"},
+            "location": {"html_tag": "div", "html_class": "companyLocation"},
+            "description": {"html_tag": "div", "html_class": "job-snippet"},
             "days_ago": {"html_tag": "span", "html_class": "date"},
-            "salary": {"html_tag": "span", "html_class": "salaryText"},
+            "salary": {"html_tag": "span", "html_class": "salary-snippet"},
+            "employer_rating": {"html_tag": "span", "html_class": "ratingNumber"},
         }
 
         for key, value in fields.items():
@@ -153,7 +154,7 @@ class IndeedScraper:
                 else:
                     matching_text = card.find(html_tag, html_class).text.strip()
             except AttributeError:
-                logger.warn(
+                logger.debug(
                     f"No HTML element match found for tag={html_tag} and class={html_class}"
                 )
                 matching_text = None
@@ -162,7 +163,7 @@ class IndeedScraper:
 
         return job_post
 
-    def post_processing(self):
+    def post_process_df(self):
         """Parses the dataframe containing all of the job posts.
 
         Duplicate entries are dropped based on the URL.
@@ -171,12 +172,12 @@ class IndeedScraper:
         """
         logger.info("Parsing posts")
         self.df = self.df.drop_duplicates("url")
+        # self.df["url"] = self.df["url"].apply(lambda x: f"=HYPERLINK('{x}'', 'URL')")
 
-    def save_posts(self):
+    def save(self):
         """Saves the job posts to CSV and Excel files"""
-        today = datetime.now().strftime("%m_%d_%y %H_%M_%S")
-        self.df.to_csv(f"../output/jobs_{today}.csv")
-        self.df.to_excel(f"../output/jobs_{today}.xlsx")
+        # today = datetime.now().strftime("%m_%d_%y %H_%M_%S")
+        self.df.to_excel(IndeedScraper.OUTPUT_FILE)
 
 
 if __name__ == "__main__":
